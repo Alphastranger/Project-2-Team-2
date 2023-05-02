1b9d4cc525a2149b69a831a4829510383e03b1d4
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-05-01 20:58:35 -0400

    Filled in the index.js

--- models/index.js
+++ models/index.js
@@ -2,4 +2,19 @@ const User = require('./User');
 const Games = require('./games');
 const Review = require('./review');
 
+User.hasMany(Review, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+Review.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+Games.hasMany(Review, {
+    foreignKey: 'games_id',
+    onDelete: 'CASCADE',
+}),
+Review.belongsTo(Games, {
+    foreignKey: 'games_id'
+})
+
 module.exports = { User, Games, Review };
